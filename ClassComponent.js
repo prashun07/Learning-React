0b8da41c16c55c289c564cc3fad12e307fa17baf
Prashun Jha <54842624+prashun07@users.noreply.github.com>
2021-06-24 17:03:27 +0530

    Create ClassComponent.js

---
+++ ClassComponent.js
@@ -0,0 +1,10 @@
+//This code show how to write function which class components method.
+
+import React, { Component } from 'react';
+
+class Welcome extends Component{
+    render() {
+        return <h1>Welcome here</h1>;
+    }
+}
+export default Welcome;
