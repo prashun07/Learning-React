783d0332a990ab50d3f605e1b28b8648fae546a4
Prashun Jha <54842624+prashun07@users.noreply.github.com>
2021-06-25 23:27:53 +0530

    using props

--- ClassComponent.js
+++ ClassComponent.js
@@ -1,10 +1,19 @@
 //This code show how to write function which class components method.
 
+// import React, { Component } from 'react';
+
+// class Welcome extends Component{
+//     render() {
+//         return <h1>Welcome here</h1>;
+//     }
+// }
+// export default Welcome;
+//Using props
 import React, { Component } from 'react';
 
 class Welcome extends Component{
     render() {
-        return <h1>Welcome here</h1>;
+        return <h1>Welcome {this.props.name}</h1>;
     }
 }
 export default Welcome;
